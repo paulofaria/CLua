12fc5899848c7289e75fcad50431493ddc9c60fe
Paulo faria <paulo@faria.id>
2021-11-25 12:40:52 -0300

    Updates

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:5.5.0
 import PackageDescription
 
 let package = Package(
-    name: "CLua"
+    name: "CLua",
+    products: [
+        .library(name: "CLua", targets: ["CLua"]),
+    ],
+    targets: [
+        .target(name: "CLua", dependencies: []),
+    ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import CLuaTestSuite
-
-XCTMain([
-     testCase(CLuaTests.allTests),
-])
